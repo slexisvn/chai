5bcb755eee0b4a2d8df6a1529403da415867b17e
Sinh <sinhtrngvn123@gmail.com>
2019-01-20 19:43:03 +0700

    update

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ var atg = document.getElementsByTagName('a');
 
 if (LN === 'vi') {
   const censp = document.querySelectorAll('.center span');
-  let _vi = ['Tính khối lượng mol', 'Cân bằng phương trình', 'Chuỗi phương trình', 'Bảng tính tan', 'Bảng tuần hoàn', 'Phương trình', 'Chất hóa học', 'Cấu trúc 2d của phân tử hữu cơ', 'Cấu trúc 3d của phân tử hữu cơ', 'Dãy điện hóa kim loại', 'Đánh vần với nguyên tố'];
+  let _vi = ['Tính khối lượng mol', 'Cân bằng phương trình', 'Chuỗi phương trình', 'Bảng tính tan', 'Bảng tuần hoàn', 'Phương trình', 'Chất hóa học', 'Cấu trúc 2d của phân tử hữu cơ', 'Cấu trúc 3d của phân tử hữu cơ', 'Đánh vần với nguyên tố', 'Dãy điện hóa kim loại'];
   for (let i = 0; i < 11; i++) {
     censp[i].innerHTML = _vi[i];
   }
